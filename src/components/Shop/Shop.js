77c69ee99557ecc4a823516cf23198a83e60e42b
Tohir Raihan <tohir.raihan@gmail.com>
2021-09-26 10:16:45 +0600

    design the product card

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,8 +1,25 @@
+import Programmer from '../Programmer/Programmer';
 import './Shop.css';
 
 const Shop = () => {
     return (
-        <div></div>
+        <section className='shop'>
+            <div className="container">
+                <div className="row">
+                    {/* programmers section */}
+                    <div className="col-md-9 programmers">
+                        <div className="row g-4">
+                            <Programmer></Programmer>
+                            <Programmer></Programmer>
+                            <Programmer></Programmer>
+                            <Programmer></Programmer>
+                        </div>
+                    </div>
+                    <div className="col-md-3">cart</div>
+                </div>
+                {/* ./row */}
+            </div>
+        </section>
     );
 }
 
