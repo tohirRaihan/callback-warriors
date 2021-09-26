a28b89153bf8587e6fd94301cc32527250cbdc0d
Tohir Raihan <tohir.raihan@gmail.com>
2021-09-26 21:36:42 +0600

    completes header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,11 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <header>
-            <div className="container my-5">
-                <h1>Make a team of Programmers</h1>
-                <p>A huge cyber attack have been strucked in the vally and we have some top notch computer genius</p>
-                <h3>Total Budget: <span className=''>100 Million</span></h3>
+        <header className='header py-5'>
+            <div className="container text-light py-5">
+                <h1 className='display-5 text-info fw-bold'>Make a team of Programmers</h1>
+                <p className='lead'>A huge cyber attack have been strucked in to the vally and we have some top notch computer genius!!!</p>
+                <h3 className='display-6'>Total Budget: <span className='fw-bold text-info'>100 Million</span></h3>
             </div>
         </header>
     );
